b8f2abc3bd35364ebb51f229aab1010af28ed75c
jakubrzeznicki <kubakensi@gmail.com>
2019-10-16 19:07:37 +0200

    initial commit, delete system.out.println

--- JavaCalculator/src/javacalculator/JavaCalculator.java
+++ JavaCalculator/src/javacalculator/JavaCalculator.java
@@ -17,8 +17,7 @@ public class JavaCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello, Calculator!");
+ 
         Scanner scanner = new Scanner(System.in); //morrison
    }    
 }
