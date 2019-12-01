2facd444ddc90388dfb1f4d959ea22010352b9a8
kacper21galantowicz@gmail.com <kacper21galant@gmail.com>
2019-12-01 15:25:23 +0100

    Searching1.1

--- AndroidCommunicator/Communicator/app/src/main/java/pl/lodz/uni/math/advanded/programming/project/communicator/SearchActivity.java
+++ AndroidCommunicator/Communicator/app/src/main/java/pl/lodz/uni/math/advanded/programming/project/communicator/SearchActivity.java
@@ -1,14 +1,91 @@
 package pl.lodz.uni.math.advanded.programming.project.communicator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseUser firebaseUser;
+    private FirebaseAuth firebaseAuth;
+    private EditText searchText;
+    private ListView searchList;
+    private ArrayAdapter adapter;
+    private ArrayList<String> names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        init();
+        LoadUsers();
+
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    private void init() {
+        searchText = findViewById(R.id.searchFilter);
+        searchList = findViewById(R.id.searchList);
+        names = new ArrayList<>();
+        adapter = new ArrayAdapter(this, R.layout.list_item, names);
+        searchList.setAdapter(adapter);
+    }
+
+    private void LoadUsers() {
+        firebaseDatabase.getReference().child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    // TODO: handle the po
+                    names.add(postSnapshot.child("username").getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Getting Post failed, log a message
+
+                // ...
+            }
+        });
+
     }
 }
