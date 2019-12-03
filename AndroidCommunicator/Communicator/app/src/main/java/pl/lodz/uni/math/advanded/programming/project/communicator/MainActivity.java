4920d37ea46f667270c265fca051d47a4e0ce5f0
jakub.rzeznicki <kubakensi@gmail,com>
2019-12-03 09:49:21 +0100

    chat update

--- AndroidCommunicator/Communicator/app/src/main/java/pl/lodz/uni/math/advanded/programming/project/communicator/MainActivity.java
+++ AndroidCommunicator/Communicator/app/src/main/java/pl/lodz/uni/math/advanded/programming/project/communicator/MainActivity.java
@@ -39,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChatListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void init() {
