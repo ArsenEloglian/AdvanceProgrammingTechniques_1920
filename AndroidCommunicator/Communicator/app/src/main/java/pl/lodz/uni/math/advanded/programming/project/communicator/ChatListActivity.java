eda4b7273c61e9538d949f95f692e4b1b014b270
Daymos <pawel.pp10@gmail.com>
2019-12-11 18:23:31 +0100

    zadanie 2,3,4

---
+++ AndroidCommunicator/Communicator/app/src/main/java/pl/lodz/uni/math/advanded/programming/project/communicator/ChatListActivity.java
@@ -0,0 +1,28 @@
+package pl.lodz.uni.math.advanded.programming.project.communicator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class ChatListActivity extends AppCompatActivity {
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+
+        mViewPager = (ViewPager) findViewById(R.id.main_tabPager);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+}
